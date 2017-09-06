9d079f991b982eb25bd53a1d6b1be36b00dfe057
Priswall <31701242+Priswall@users.noreply.github.com>
2017-09-06 12:48:51 -0400

    Update testing.js

--- testing.js
+++ testing.js
@@ -2,6 +2,13 @@ window.reequestanimationframe(loop);
 var canvas = document.getElementById("canvas");
 canvas.getContext("2d");
 
+var cats = 0;
+var owneditems = [0,0,0,0,0];
+
+c.fillStyle = "black";
+c.fillText("CATS: "+cats);
+
 function loop(){
+  c.fillStyle = "light-blue";
   c.fillRect(0,0,640,800);
 }
