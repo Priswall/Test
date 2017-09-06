f2cccf8c7189919a5abcbba9b1705f8b2909316d
Priswall <31701242+Priswall@users.noreply.github.com>
2017-09-06 17:11:05 -0400

    Update testing.js

--- testing.js
+++ testing.js
@@ -3,6 +3,8 @@ var c = canvas.getContext("2d");
 c.font = "20px Arial";
 
 var cats = 0;
+var clickamount = 0;
+var clicks = 0;
 var owneditems = [0,0,0,0,0];
 var itemprice = [50,500,5000,10000,100000];
 var itemname = ["Kitten","Cat","Cat family","Nekomimi","Catman"];
@@ -14,29 +16,47 @@ function loop(){
   canvas.height = window.innerHeight;
   
   c.fillStyle = "lightblue";
-  c.fillRect(0,0,640,800);
+  c.fillRect( 0, 0, 640, 800 );
   
-  for(var i = 0; i < owneditems.length; i++){
-    c.fillStyle = "rgb(0,"+(100+(i*30))+","+(100+(i*30))+")";
-    c.fillRect(400, 200+(75*i),175,50);
+  for( var i = 0; i < owneditems.length; i++ ){
+    c.fillStyle = "rgb(0," + ( 100 + ( i * 30 ) ) + "," + ( 100 + ( i * 30 ) ) + ")";
+    c.fillRect( 400, 200 + ( 75 * i ), 175, 50 );
     c.fillStyle = "black";
     c.font = "20px Arial";
-    c.fillText(itemname[i], 425, 230+(75*i));
+    c.fillText( itemname[i], 425, 230 + ( 75 * i ) );
     c.font = "10px Arial";
-    c.fillText("Owned: "+owneditems[i], 400, 257+(75*i));
+    c.fillText( "Owned: " + owneditems[i], 400, 257 + ( 75 * i ) );
   }
   
   c.fillStyle = "black";
-  c.fillText("CATS: "+cats, 500+(cats*8),50);
+  c.fillText( "CATS: " + cats, 500 + ( cats * 8 ), 50 );
   
   window.requestAnimationFrame(loop);
-}
+}                            // End Loop
 
 function persecond(){
-  for(var i = 0; i < owneditems.length; i++){
+  for( var i = 0; i < owneditems.length; i++ ){
     cats+=(owneditems[i]*itemcps[i]);
   }
 }
+
+function mouseclicked(e) {
+  for( var i = 0; i < owneditems.length; i++ ){
+    if( e.clientY < 250 + ( 75 * i ) && e.clientY > 200 + ( 75 * clientY ) && e.clientX > 400 && e.clientX < 575 ){
+      if( money >= itemprice[i] ){
+        money -= itemprice;
+        itemprice += Math.floor( ( 0.6 * owneditems[i] ) + ( 0.4 * owneditems[i] ) );
+      } else {
+        //Insert easter egg here
+      }
+    } else if( e.clientX > 475 && e.clientX < 600 && e.clientY > 40 && e.clientY < 165){
+      cats += clickamount;
+      clicks++;
+    }
+  }
+}
+
+window.addEventListener("click",mouseclicked);
 
 window.setInterval(persecond,1000);
 window.requestAnimationFrame(loop);
