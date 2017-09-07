3c25d26d2f865976ab409533af106b7ce544e274
Priswall <31701242+Priswall@users.noreply.github.com>
2017-09-07 14:45:56 -0400

    Update testing.js

--- testing.js
+++ testing.js
@@ -22,14 +22,17 @@ function loop(){
     c.fillStyle = "rgb(0," + ( 100 + ( i * 30 ) ) + "," + ( 100 + ( i * 30 ) ) + ")";
     c.fillRect( ( canvas.width / 3 ) * 2, ( canvas.height / 3 ) + ( ( canvas.height / 8 ) * i ), ( canvas.width / 3 ) - ( canvas.width / 8 ), canvas.height / 16 );
     c.fillStyle = "black";
-    c.font = "20px Arial";
+    c.font = ( canvas.height / 30 ) + "px Arial";
     c.fillText( itemname[i], ( ( canvas.width / 3 ) * 2 ) + ( canvas.width / 12 ), ( ( canvas.height / 3 ) + ( canvas.height / 24 ) )+ ( ( canvas.height / 8 ) * i ) );
-    c.font = "10px Arial";
+    c.font = ( canvas.height / 20 ) + "px Arial";
     c.fillText( "Owned: " + owneditems[i], ( ( canvas.width / 3 ) * 2 ) + ( canvas.width / 12 ), ( ( canvas.height / 3 ) + ( canvas.height / 12 ) )+ ( ( canvas.height / 8 ) * i ) );
   }
   
+  c.fillStyle = "rgb( 0, 255, 255 )";
+  c.fillRect( ( canvas.width / 3 ) * 2, 0, canvas.width / 3, canvas.height / 3 );
+  
   c.fillStyle = "black";
-  c.fillText( "CATS: " + cats, 500 + ( cats * 8 ), 50 );
+  c.fillText( "CATS: " + cats, canvas.width / 2 - ( cats * ( canvas.height / 10 ) ), canvas.height / 10 );
   
   window.requestAnimationFrame(loop);
 }                            // End Loop
@@ -42,14 +45,14 @@ function persecond(){
 
 function mouseclicked(e) {
   for( var i = 0; i < owneditems.length; i++ ){
-    if( e.clientY < 250 + ( 75 * i ) && e.clientY > 200 + ( 75 * e.clientY ) && e.clientX > 400 && e.clientX < 575 ){
+    if( e.clientY < ( ( canvas.height / 3 ) + ( ( canvas.height / 8 ) * i ) ) + ( canvas/height / 16 ) && e.clientY > ( canvas.height / 3 ) + ( ( canvas.height / 8 ) * i ) && e.clientX > ( canvas.width / 3 ) * 2 && e.clientX < ( ( canvas.width / 3 ) * 2 ) + ( canvas.width / 3 ) - ( canvas.width / 8 ) ){
       if( cats >= itemprice[i] ){
         cats -= itemprice;
         itemprice += Math.floor( ( 0.6 * owneditems[i] ) + ( 0.4 * owneditems[i] ) );
       } else {
         //Insert easter egg here
       }
-    } else if( e.clientX > 475 && e.clientX < 600 && e.clientY > 40 && e.clientY < 165){
+    } else if( e.clientX > ( canvas.width / 3 ) * 2 && e.clientX < canvas.width && e.clientY > 0 && e.clientY < canvas.height / 3 ){
       cats += clickamount;
       clicks++;
     }
