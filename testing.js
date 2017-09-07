9a3d0ca773da994d0fce946f8faa0a6929cb60a0
Priswall <31701242+Priswall@users.noreply.github.com>
2017-09-07 14:57:51 -0400

    Update testing.js

--- testing.js
+++ testing.js
@@ -59,7 +59,18 @@ function mouseclicked(e) {
   }
 }
 
+function mousemoved() {
+  
+  for( var i = 0; i < owneditems.length; i++ ){
+    if( e.clientY < ( ( canvas.height / 3 ) + ( ( canvas.height / 8 ) * i ) ) + ( canvas.height / 16 ) && e.clientY > ( canvas.height / 3 ) + ( ( canvas.height / 8 ) * i ) && e.clientX > ( canvas.width / 3 ) * 2 && e.clientX < ( ( canvas.width / 3 ) * 2 ) + ( canvas.width / 3 ) - ( canvas.width / 8 ) ){
+      canvas.syle.cursor = "pointer";
+    } else{
+      canvas.style.cursor = "auto";
+    }
+}
+
 window.addEventListener("click",mouseclicked);
+window.addEventListener("mousemove",mousemoved);
 
 window.setInterval(persecond,1000);
 window.requestAnimationFrame(loop);
