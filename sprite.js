405f1478e09034b754a009ab5a38517027505e11
Priswall <31701242+Priswall@users.noreply.github.com>
2017-10-02 12:56:28 -0400

    Update sprite.js

--- sprite.js
+++ sprite.js
@@ -20,19 +20,19 @@ function makeSprites( img ){
   cat = [
     new Sprite( img, 0, 0, 17, 17 ),
     new Sprite( img, 18, 0, 17, 17 ),
-    new Sprite( img, 27, 0, 17, 17 ),
+    new Sprite( img, 27, 0, 17, 17 )
   ];
   
   printt = new Sprite( img, 52, 0, 11, 9 );
     
   icons[
     
-    new Sprite( img, 36, 17, 10, 10 );
-    new Sprite( img, 51, 9, 8, 6 );
-    new Sprite( img, 60, 9, 6, 6 );
-    new Sprite( img, 46, 17, 5, 5 );
-    new Sprite( img, 52, 17, 5, 5 );
-    new Sprite( img, 58, 17, 5, 5 );
+    new Sprite( img, 36, 17, 10, 10 ),
+    new Sprite( img, 51, 9, 8, 6 ),
+    new Sprite( img, 60, 9, 6, 6 ),
+    new Sprite( img, 46, 17, 5, 5 ),
+    new Sprite( img, 52, 17, 5, 5 ),
+    new Sprite( img, 58, 17, 5, 5 )
       
   ];
 
